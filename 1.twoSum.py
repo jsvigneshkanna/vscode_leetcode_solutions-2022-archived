0e21bfe3e8f8be040a8099a79bfdc888064f9aaa
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-12 13:04:02 +0530

    testfile added

---
+++ 1.twoSum.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print('fafa')
+        
+# @lc code=end
+
