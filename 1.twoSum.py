8acb896ccb1d36f8de84c6804da1681f6f330c6e
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-12 13:12:43 +0530

    two-sum submitted

--- 1.twoSum.py
+++ 1.twoSum.py
@@ -2,12 +2,19 @@
 # @lc app=leetcode id=1 lang=python3
 #
 # [1] Two Sum
-#
+# submission=12/11/22
 
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        print('fafa')
+        hashmap = {}
+        for index, num in enumerate(nums):
+            if target-num in hashmap:
+                return [index, hashmap.get((target-num))]
+            else:
+                hashmap[num] = index
+        return [-1, -1]
+        
         
 # @lc code=end
 
