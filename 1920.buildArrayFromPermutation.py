f7d5e9fe9a663b913dd6191e5310ecc1e64bb9c0
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-14 22:42:36 +0530

    1920 python accepted

---
+++ 1920.buildArrayFromPermutation.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1920 lang=python3
+#
+# [1920] Build Array from Permutation
+#
+
+# @lc code=start
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        # Method 1 additional space
+        length = len(nums)
+        permutation_arr  = [0]* length
+        for index in range(length):
+            permutation_arr[index] = nums[nums[index]]
+        return permutation_arr
+        
+# @lc code=end
+
