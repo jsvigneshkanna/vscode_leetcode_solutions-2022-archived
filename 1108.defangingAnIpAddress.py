c3b2e249643686b83810474d8d4372106ac8a532
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-15 00:20:16 +0530

    1108 oython optimal space accepted

--- 1108.defangingAnIpAddress.py
+++ 1108.defangingAnIpAddress.py
@@ -7,14 +7,19 @@
 # @lc code=start
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        defanged_address = ''
-        for char in address:
-            if char == '.':
-                defanged_address += '[.]'
-            else:
-                defanged_address += char
+        # Method 1 o(n) space
 
-        return defanged_address    
+        # defanged_address = ''
+        # for char in address:
+        #     if char == '.':
+        #         defanged_address += '[.]'
+        #     else:
+        #         defanged_address += char
+
+        # return defanged_address    
+
+        # Method 2 O(1) space
+        return "[.]".join(address.split('.'))
         
 # @lc code=end
 
