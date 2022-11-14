585dd13daecb21dc9ff40c121ca1e0693e7714d8
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-14 23:48:09 +0530

    1920 optimal space oy accepted

--- 1920.buildArrayFromPermutation.py
+++ 1920.buildArrayFromPermutation.py
@@ -8,11 +8,24 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         # Method 1 additional space
+
+        # length = len(nums)
+        # permutation_arr  = [0]* length
+        # for index in range(length):
+        #     permutation_arr[index] = nums[nums[index]]
+        # return permutation_arr
+
+        # Method 2 (optimal space)
+        max_weight = 1001
         length = len(nums)
-        permutation_arr  = [0]* length
         for index in range(length):
-            permutation_arr[index] = nums[nums[index]]
-        return permutation_arr
+            nums[index] = nums[index] + (max_weight* (nums[nums[index]] % max_weight))
+            print(nums[index])
+            
+        for index in range(length):
+            nums[index] = nums[index] // max_weight
+        
+        return nums
         
 # @lc code=end
 
