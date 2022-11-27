ee247016b6736af14238a433da049846accbb69e
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-27 11:10:08 +0530

    217 python accepted

---
+++ 217.containsDuplicate.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+# @lc code=end
+
